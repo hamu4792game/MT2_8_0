08c453886c28321d451ed47fd83750dc81086505
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2023-02-16 21:50:35 +0900

    摩擦の追加

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 ﻿#include <Novice.h>
 #include "Matrix3x3.h"
+#include <math.h>
 
 const char* kWindowTitle = "MT2_13_0_確認";
 
@@ -7,7 +8,7 @@ const char* kWindowTitle = "MT2_13_0_確認";
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
 
 	// ライブラリの初期化
-	int kWindowWidth = 480, kWindowHeight = 960;
+	int kWindowWidth = 1280, kWindowHeight = 720;
 	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
 	// キー入力結果を受け取る箱
@@ -15,7 +16,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR lpCmdLine, _In
 	char preKeys[256] = {0};
 
 	//	行列の作成
-	Matrix3x3 cameraMatrix = cameraMatrix.MakeAffineMatrix({ 1.0f,1.0f }, 0.0f, { 240.0f,480.0f });
+	Matrix3x3 cameraMatrix = cameraMatrix.MakeAffineMatrix({ 1.0f,1.0f }, 0.0f, { 440.0f,160.0f });
 
 	Matrix3x3 viewMatrix = viewMatrix.Inverse(cameraMatrix);
 
@@ -26,32 +27,30 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR lpCmdLine, _In
 	Matrix3x3 wvpVpMatrix;
 
 	Vec2 gravity = { 0.0f,-9.8f };
+	float miu = 0.4f;
+	float magnitude = 0.0f;
+	Vec2 direction = { 0.0f,0.0f };
+	Vec2 frictionalForce = { 0.0f,0.0f };
 
-	struct Ball
+
+	struct Box
 	{
 		Vec2 position;		//	ボールの位置
+		Vec2 size;			//	ボールの半径
 		Vec2 velocity;		//	ボールの速度
 		Vec2 acceleration;	//	ボールの加速度
 		float mass;			//	ボールの質量
-		float radius;		//	ボールの半径
 		unsigned int color;	//	ボールの色
 	};
-	Ball ball1{
-		{320.0f,960},
+	Box box{
+		{50.0f,50.0f},
+		{100.0f,100.0f},
 		{0.0f,0.0f},
-		gravity,
-		1.0f,
+		{0.0f,0.0f},
 		10.0f,
 		0xffffffff
 	};
-	Ball ball2{
-		{160.0f,960},
-		{0.0f,0.0f},
-		gravity,
-		1.0f,
-		10.0f,
-		0xffffffff
-	};
+
 
 	bool flag = false;
 
@@ -70,30 +69,40 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR lpCmdLine, _In
 		/// ↓更新処理ここから
 		///
 
-		if (keys[DIK_SPACE])
+		if (keys[DIK_SPACE] != 0 && preKeys[DIK_SPACE] == 0)
 		{
+			box.velocity.x = 70.0f;
 			flag = true;
 		}
 
 		if (flag)
 		{
-			//	メインループでボールの速度に加速度を足す
-			
-			ball1.velocity.y += (ball1.acceleration.y / 60.0f);
 
-			ball1.position.y += (ball1.velocity.y / 60.0f);
+			magnitude = miu * (-box.mass * gravity.y);
 
-			Vec2 airResistance = {
-				0.6f * -ball2.velocity.x,0.6f * -ball2.velocity.y
-			};
+			if (box.velocity.x > 0.0f)
+			{
+				direction.x = -1.0f;
+			}
+			else if (box.velocity.x < 0.0f)
+			{
+				direction.x = 1.0f;
+			}
 
-			Vec2 airResistanceAcceleration = { 0.0f,airResistance.y / ball2.mass };
+			frictionalForce = { magnitude * direction.x,0.0f };
 
-			ball2.acceleration.y = gravity.y + airResistanceAcceleration.y;
+			box.acceleration.x = frictionalForce.x / box.mass;
 
-			ball2.velocity.y += (ball2.acceleration.y / 60.0f);
 
-			ball2.position.y += (ball2.velocity.y / 60.0f);
+			if (fabsf(box.acceleration.x / 60.0f) > fabsf(box.velocity.x))
+			{
+				box.acceleration.x = box.velocity.x * 60.0f;
+			}
+
+			box.velocity.x += (box.acceleration.x / 60.0f);
+
+			box.position.x += (box.velocity.x / 60.0f);
+
 		}
 
 		
@@ -110,13 +119,22 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR lpCmdLine, _In
 		///
 		
 		//	座標変換
-		Vec2 screenPos1 = Transform(ball1.position, wvpVpMatrix);
-		Vec2 screenPos2 = Transform(ball2.position, wvpVpMatrix);
+		Vec2 screenPos = Transform(box.position, wvpVpMatrix);
+		
+		Vec2 screenLine1 = Transform({ static_cast<float>(-(kWindowWidth)),0.0f }, wvpVpMatrix);
+		Vec2 screenLine2 = Transform({ static_cast<float>((kWindowWidth)),0.0f }, wvpVpMatrix);
+		Vec2 screenLine3 = Transform({ 0.0f,static_cast<float>(-(kWindowHeight)) }, wvpVpMatrix);
+		Vec2 screenLine4 = Transform({ 0.0f,static_cast<float>((kWindowHeight)) }, wvpVpMatrix);
 
-		Novice::DrawEllipse(static_cast<int>(screenPos1.x), static_cast<int>(screenPos1.y),
-			static_cast<int>(ball1.radius), static_cast<int>(ball1.radius), 0.0f, ball1.color, kFillModeSolid);
-		Novice::DrawEllipse(static_cast<int>(screenPos2.x), static_cast<int>(screenPos2.y),
-			static_cast<int>(ball2.radius), static_cast<int>(ball2.radius), 0.0f, ball2.color, kFillModeSolid);
+		Novice::DrawLine(screenLine1.x, screenLine1.y, screenLine2.x, screenLine2.y, 0xff0000ff);
+		Novice::DrawLine(screenLine3.x, screenLine3.y, screenLine4.x, screenLine4.y, 0x00ff00ff);
+
+
+		Novice::DrawQuad(screenPos.x - (box.size.x / 2), screenPos.y - (box.size.y / 2),
+			screenPos.x + (box.size.x / 2), screenPos.y - (box.size.y / 2),
+			screenPos.x - (box.size.x / 2), screenPos.y + (box.size.y / 2),
+			screenPos.x + (box.size.x / 2), screenPos.y + (box.size.y / 2),
+			0, 0, 1, 1, white, box.color);
 
 		///
 		/// ↑描画処理ここまで
